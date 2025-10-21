17f14a42be3f808fd54e8c7c7c4b0db4a841a0eb
Ian <ian.yucra@utec.edu.pe>
2025-10-21 18:21:02 -0500

    base

---
+++ hack1/src/test/java/org/example/hack1/Hack1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hack1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hack1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
