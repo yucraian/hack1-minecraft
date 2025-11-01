437ce89d00dbe32fd12cf3443335d827e91214c5
Tu Nombre <correo@ejemplo.com>
2025-11-01 18:52:54 -0500

    ahora si funciona postman

--- hack1/src/main/java/org/example/hack1/user/domain/User.java
+++ hack1/src/main/java/org/example/hack1/user/domain/User.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
